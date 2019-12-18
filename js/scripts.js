c71ad9026ac3f5aa6ea886bac9c1018487a4e522
Francesco <fraiolinotes@gmail.com>
2019-12-17 22:08:57 -0500

    more style added

--- js/scripts.js
+++ js/scripts.js
@@ -21,45 +21,55 @@ function draw(data, tabletop) {
 
     d3.selectAll(".location")
         .append("text")
-        .attr("class", function(d) { 
-            if(d.delivered == "TRUE"){
+        .attr("class", function(d) {
+            if(d.delivered == "TRUE" && d.driverNote){
+                return "doneWithIssues"
+            } else if(d.delivered == "FALSE" && d.driverNote){
+                return "justIssues"
+            } else if(d.delivered == "TRUE"){
                 return "done"
-            } else {
+            } else if(d.delivered == "FALSE"){
                 return "inProgress"
             }
         })
-        .text(function(d) { 
-            return d.locationName;
-        })
+        .text(function(d) { return d.locationName; });
 
     d3.selectAll(".location")
-        .append("svg:image")
-        .attr("class", function(d) { 
-            if(d.delivered == "TRUE"){
-                return "pic"
+        .append("p")
+        .html(function(d) {return d.driverNote; })
+        .attr("class", function(d){
+            if(d.driverNote){
+                return "showNote";
             } else {
-                return "noPic"
+                return "hideNote";
             }
-        })
-        .attr({
-            "xlink:href": function(d) { return d.confirmationPic; },
-            x: 0,
-            y: 0,
-            width: 20,
-            height: 20
         });
 
-    var svg = d3.select("#map"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+    d3.selectAll(".location")
+        .append("a")
+        .attr("href", function(d){
+            return d.confirmationPic;
+        })
+        .html("📎")
+        .attr("class", function(d){
+            if(d.confirmationPic){
+                return "showPic";
+            } else {
+                return "hidePic";
+            }
+        });
 
-    
+
+    function deliveryAlerts(data) {
+        if(data.delivered == "TRUE" && data.issues == "TRUE"){
+            return "doneWithIssues"
+        } else if(data.delivered == "FALSE" && data.issues == "TRUE"){
+            return "justIssues"
+        } else if(data.delivered == "FALSE" && data.issues == "FALSE"){
+            return "inProgress"
+        }
+    }
     //nyc();
-
-
-    
-    
-
 }
 
 renderSpreadsheetData();
